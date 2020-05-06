62259ca28c6338d4c660d55be6936393226a9e29
SaintHuddini <Salah1799@hotmail.com>
2020-05-06 15:31:43 +0000

    User can see all Diarys

--- app.py
+++ app.py
@@ -4,11 +4,18 @@ from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
 
+
+diarys = [
+    { 'title': 'Hello world!', 'mood': 'happy', 'entry': 'A new day and a new beginning.'},
+    { 'title': 'A buggy day', 'mood': 'tired', 'entry': 'Full of bugs and roadblocks.'}
+]
+
+
 @app.route('/')
 @app.route('/home')
 def diary():
-    # Shows the playlists
-    return render_template('diary.html')
+    # Shows user the diary
+    return render_template('diary.html', diarys=diarys)
 
 
 if __name__ == "__main__":
