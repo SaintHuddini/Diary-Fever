60cef4fdb450727f33986843d8330028f0bc7494
SaintHuddini <Salah1799@hotmail.com>
2020-05-10 18:16:03 +0000

    Bootstrap cdn and custom css added to the site

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request, url_for, redirect
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 import env
+import datetime
 
 
 app = Flask(__name__)
